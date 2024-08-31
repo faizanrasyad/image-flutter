631169813bc0842ed3eb582de97c60b4deed4637
faizanrasyad <rasyadfaizan@gmail.com>
2024-08-31 19:35:57 +0700

    Tampilan Home Page

---
+++ lib/pages/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Upload Image Flutter',
+          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+        ),
+        backgroundColor: Colors.green,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Image.asset(
+          'assets/note_stack_add.png',
+          width: 40,
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text(
+              'List Movies',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: 1, itemBuilder: ((context, index) {})))
+          ],
+        ),
+      )),
+    );
+  }
+}
