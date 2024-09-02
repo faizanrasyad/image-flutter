ddbf60aa4a1bbf7ed5729bc8b88e1df598201002
faizanrasyad <rasyadfaizan@gmail.com>
2024-09-02 15:00:42 +0700

    Movie Detail DONE

--- lib/pages/movie_detail.dart
+++ lib/pages/movie_detail.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_flutter/networking/models/movies_model.dart';
 
@@ -12,6 +13,8 @@ class MovieDetail extends StatefulWidget {
 class _MovieDetailState extends State<MovieDetail> {
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+
     Movies movie = widget.movie;
     return Scaffold(
       appBar: AppBar(
@@ -37,10 +40,48 @@ class _MovieDetailState extends State<MovieDetail> {
             child: Padding(
               padding: EdgeInsets.all(16),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Center(
-                    child: Text("Movie Name: ${movie.name}"),
-                  )
+                    child: Container(
+                      child: ClipRRect(
+                        child: Image.network(
+                          movie.image,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    movie.name,
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.start,
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.movie,
+                        color: Colors.green,
+                        size: 16,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        movie.production,
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
